6f68ac4849af2e533efc1dea7195a3e0f895f36b
Bri in The Sky <brianh6854@googlemail.com>
2016-04-26 21:48:44 +0100

    Count rows working

--- quick.cr
+++ quick.cr
@@ -4,9 +4,13 @@ db = "threatmonitor"
 
 mid = MonetDBMAPI.mapi_connect("172.17.0.2", 50000, "monetdb", "monetdb", "sql", db);
 p mid
-hdl = MonetDBMAPI.mapi_query(mid, "SELECT 1")
+hdl = MonetDBMAPI.mapi_query(mid, "SELECT NOW() UNION ALL SELECT NOW()")
 p hdl
-res = MonetDBMAPI.mapi_fetch_row(hdl)
-p res
-
+lang = MonetDBMAPI.mapi_get_lang(mid)
+p lang
+count = MonetDBMAPI.mapi_get_row_count(hdl)
+p count
+#while(line = MonetDBMAPI.mapi_fetch_row(hdl))
+#  puts line.to_s
+#end
 
