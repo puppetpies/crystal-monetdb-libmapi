40fe7c9414baa056fdf7f181487144bb60465cb5
Bri in The Sky <brianh6854@googlemail.com>
2016-04-26 16:01:51 +0000

    Works yay!

--- quick.cr
+++ quick.cr
@@ -1,6 +1,12 @@
 require "./src/monetdb"
 
 db = "threatmonitor"
-mid = MonetDBMAPI.mapi_connect("172.17.0.7", 50000, "monetdb", "monetdb", "sql", db);
+
+mid = MonetDBMAPI.mapi_connect("172.17.0.2", 50000, "monetdb", "monetdb", "sql", db);
 p mid
+hdl = MonetDBMAPI.mapi_query(mid, "SELECT 1")
+p hdl
+res = MonetDBMAPI.mapi_fetch_row(hdl)
+p res
+
 
