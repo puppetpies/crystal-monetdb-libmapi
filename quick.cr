06c5e44e1389834399f853bbaa97cf0e0b62622c
Bri in The Sky <brianh6854@googlemail.com>
2016-04-28 07:54:26 +0100

    Added option_parser and custom Exception handlers

--- quick.cr
+++ quick.cr
@@ -11,6 +11,10 @@
 
 require "./src/monetdb"
 require "colorize"
+require "option_parser"
+
+class ConnectionError < Exception; end
+class QueryError < Exception; end
 
 def random_alphabet
   a = "abcdefghijklmnopqrstuvwxyz"
@@ -22,54 +26,131 @@ def random_alphabet
   return c
 end
 
-mero = MonetDB.new
-mero.host = "172.17.0.2"
-mero.db = "threatmonitor"
+def print_stamp
+  puts "Author: Brian Hood"
+  puts "Homepage: https://github.com/puppetpies/crystal-monetdb-libmapi"
+end
 
-puts "Merovingian Server: #{mero.host}"
-puts "Port: #{mero.port}"
-puts "Username: #{mero.username}"
-puts "DB: #{mero.db}"
+host = "127.0.0.1"
+port = 50000
+username = "monetdb"
+password = "monetdb"
+db = "test"
+insloop = 3_000
+displayinterval = 250
+
+mero = MonetDB.new
+oparse = OptionParser.parse! do |parser|
+  parser.banner = "Usage: quick [options]"
+
+  parser.on("-H 127.0.0.1", "--HOST=127.0.0.1", "\tIP / DNS Name") {|f|
+    mero.host = f
+  }
+  parser.on("-P 50000", "--PORT=50000", "\tTCP Port") {|f|
+    mero.port = f.to_i
+  }
+  parser.on("-u monetdb", "-USERNAME=monetdb", "\tUsername") {|f|
+    mero.username = f
+  }
+  parser.on("-p monetdb", "-PASSWORD=monetdb", "\tPassword") {|f|
+    mero.password = f
+  }
+  parser.on("-d database", "-DB=test", "\tDatabase") {|f|
+    mero.db = f
+  }
+  parser.on("-l ITERATIONS", "-LOOP=3000", "\tINSERT Iterations") {|f|
+    insloop = f.to_i
+  }
+  parser.on("-i INTERVAL", "-INTERVAL=250", "\tDisplay Interval") {|f|
+    displayinterval = f.to_i
+  }
+  parser.on("-h", "--help", "Show this help") {|h|
+    puts parser
+    puts
+    print_stamp
+    exit 0
+  }
+end
+oparse.parse!
+
+mero.host ||= host
+mero.port ||= port
+mero.username = username
+mero.db ||= password
+
+
+puts "Merovingian Server: #{mero.host}".colorize(:blue)
+puts "Port: #{mero.port}".colorize(:blue)
+puts "Username: #{mero.username}".colorize(:blue)
+puts "DB: #{mero.db}".colorize(:blue)
  
 mid = mero.connect # Connect to a MServer5
 isc = mero.is_connected?(mid)
-puts "Is Connected?: #{isc}"
-puts "Mid: #{mid}"
+puts "Is Connected?: #{isc}".colorize(:blue)
+ping = mero.ping(mid)
+puts "Ping?: #{ping}".colorize(:blue)
+puts "Mid: #{mid}".colorize(:blue)
 uri = mero.get_uri(mid)
-puts "Merovingian URI: #{uri}"
+puts "Merovingian URI: #{uri}".colorize(:blue)
 ver = mero.get_monet_version(mid)
-puts "Monet Version: #{ver.to_s}"
+puts "Monet Version: #{ver.to_s}".colorize(:blue)
 puts ">> Insert Test".colorize(:red)
-num = 3_000
-puts "Start #{num}"
+if isc == false
+  raise ConnectionError.new "Not connected make sure the MServer is started and see ./quick -h for details"
+  exit
+end
+
+puts "Number of INSERT iterations: #{insloop}"
 c = 0
 mero.setAutocommit(mid, false)
-alpha = random_alphabet
-num.times {|n|
-  print "Query number: #{n} " if c == 500
+insloop.times {|n|
+  alpha = random_alphabet
+  print "Query number: #{n} " if c == displayinterval
   sql = "INSERT INTO \"threatmonitor\".guid_test VALUES ('#dummy-#{alpha}', '#{alpha}', '#{alpha}', '#{alpha}', '#{alpha}', '#{alpha}', '#{alpha}', '#{alpha}', '#{alpha}', '#{alpha}', '#{alpha}')"
   hdl = mero.query(mid, sql)
-  puts "SQL: #{sql}" if c == 500
-  if c == 500; c = 0; end
+  puts "SQL: #{sql}".colorize(:green) if c == displayinterval
+  if c == displayinterval
+    c = 0;
+  end
   c += 1
 }
-puts ">> Update Test".colorize(:green)
-sql = "UPDATE \"threatmonitor\".guid_test SET guid = 'gummybears' WHERE f3 LIKE '%sdf%';"
+mero.query(mid, "COMMIT;");
+
+puts ">> Update Test".colorize(:red)
+sql = "UPDATE \"threatmonitor\".guid_test SET guid = 'Dagobert' WHERE f#{rand(10)} LIKE '%asd%';"
+puts sql.colorize(:green)
 hdl = mero.query(mid, sql)
 aft = mero.rows_affected(hdl)
-puts "Rows affected: #{aft}"
+puts "Rows affected: #{aft}".colorize(:blue)
+hdl = mero.query(mid, "COMMIT;")
+
+puts ">> Delete Test".colorize(:red)
+sql = "DELETE FROM \"threatmonitor\".guid_test WHERE guid = 'Dagobert';"
+puts sql.colorize(:green)
+hdl = mero.query(mid, sql)
+aft = mero.rows_affected(hdl)
+puts "Rows affected: #{aft}".colorize(:blue)
+hdl = mero.query(mid, "COMMIT;")
+
+puts ">> Delete Test Empty Table".colorize(:red)
+sql = "DELETE FROM \"threatmonitor\".guid_test;"
+puts sql.colorize(:green)
+hdl = mero.query(mid, sql)
+aft = mero.rows_affected(hdl)
+puts "Rows affected: #{aft}".colorize(:blue)
 hdl = mero.query(mid, "COMMIT;")
 
 query = "SELECT * FROM \"threatmonitor\".fruits"
-puts "SELECT Statement: #{query}"
+puts "SELECT Statement: #{query}".colorize(:green)
 hdl = mero.query(mid, query)
-puts "Handle: #{hdl}"
+
+puts "Handle: #{hdl}".colorize(:blue)
 tblwidth = mero.fetch_row(hdl)
-puts "Table Width: #{tblwidth}"
+puts "Table Width: #{tblwidth}".colorize(:blue)
 count = mero.get_row_count(hdl)
-puts "Record Count: #{count}"
+puts "Record Count: #{count}".colorize(:blue)
 res = mero.execute(hdl)
-puts "Response Code: #{res}"
+puts "MServer Response Code: #{res}".colorize(:blue)
 if res == MonetDBMAPI::MOK
   0.upto(count) {|n|
     fruit = mero.fetch_field(hdl, 0)
@@ -82,10 +163,10 @@ if res == MonetDBMAPI::MOK
     mero.disconnect(mid)  # Disconnect from server
     mero.destroy(mid) # Free handle resources
     isc = mero.is_connected?(mid) # Check we disconnected
-    puts "Connection closed ? #{isc}"
+    puts "Connected to MServer ? #{isc}"
   rescue
-    Exception.new "Something went wrong closing down..."
+    raise ConnectionError.new "Something went wrong closing down..."
   end
 elsif res == MonetDBMAPI::MSERVER
-  Exception.new "Query generated and invalid response please check your SQL"
+  raise QueryError.new "Query generated and invalid response please check your SQL"
 end
