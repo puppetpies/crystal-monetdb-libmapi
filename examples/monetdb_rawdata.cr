78684d24acbbb623b10e7bef970a389f32488576
Bri in The Sky <brianh6854@googlemail.com>
2016-05-01 21:35:41 +0100

    Now generated JSON from RAW Mode output

--- examples/monetdb_rawdata.cr
+++ examples/monetdb_rawdata.cr
@@ -37,13 +37,6 @@ File.open("monetdb_raw_result.txt", "r") {|n|
   }
 }
 
-#result = String.build do |io|
-#  io.json_object do |object|
-#    object.field "address", "Crystal Road 1234"
-#    object.field "location" 
-#  end
-#end
-
 b_fields = Array(String).new
 fields.split(",").each {|n| b_fields << n.strip }
 b_types = Array(String).new
@@ -52,17 +45,47 @@ types.split(",").each {|n| b_types << n.strip }
 puts "Header Data:\n"
 puts "#{monetdb_hdr_data}"
 
-t = 0
-b_fields.each {|n|
-  puts "Field: #{n} Type: #{b_types[t]}"
-  t += 1
+puts "\nRaw Data:\n"
+monetdb_raw_data.each_line {|n|
+  puts n
 }
 
-puts "\nRaw Data:\n"
-#puts "#{monetdb_raw_data}"
-puts b_fields.join(" | ")
+puts "\nGenerated JSON Data:\n"
+t = 0
+puts "JSON Fields / Types:\n"
+  result = String.build do |io|
+    io.json_object do |object|
+    b_fields.each {|n|
+      object.field "#{n}", "#{b_types[t]}"
+      t += 1
+    }
+    end
+  end
+puts result
+
+puts "\nJSON Data:\n"
+rowcounter = 0
 monetdb_raw_data.each_line {|n|
-  #a = Tuple.new(n.gsub("NULL", "\"NULL\"").gsub("[", "").gsub("]", "").gsub("\t", "").gsub("\\", "").strip)
-  str = n.gsub("\t", "").gsub("\\\"", "").gsub("\n", "").gsub("NULL", "\"NULL\"").gsub("[ ", "").gsub("[", "").gsub("]", "")
-  puts str
+  comma_sep = Array(String).new
+  mraw = 0
+  nextrec = 0
+  comma_sep << n.gsub("\t", "").gsub("\\\"", "").gsub("\n", "").gsub("NULL", "\"NULL\"").gsub("[ ", "").gsub("[", "").gsub("]", "")
+  #puts comma_sep
+  result = String.build do |io|
+    io.json_object do |object|
+      fields.split(",").each {|field|
+        object.field "#{field.strip.gsub("\"", "")}", "#{comma_sep[nextrec].split(",")[mraw].strip.gsub("\"", "")}"
+        mraw += 1
+      }
+      rowcounter += 1
+    end
+  end
+  puts result
 }
+
+result = String.build do |io|
+  io.json_object do |object|
+    object.field "rowcount", "#{rowcounter}"
+  end
+end
+puts result
