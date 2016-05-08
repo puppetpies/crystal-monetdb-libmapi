eb9395a3598be42bf30d0eb20b8846d54dcac1b5
Bri in The Sky <brianh6854@googlemail.com>
2016-05-08 21:16:44 +0100

    Just temporary to make something work

--- src/monetdb_data.cr
+++ src/monetdb_data.cr
@@ -10,6 +10,11 @@
 ########################################################################
 require "json"
 
+class ConnectionError < Exception; end
+class QueryError < Exception; end
+class InternalError < Exception; end
+class TimeoutError < Exception; end
+
 module MonetDB
 
   class ClientJSON < Client
