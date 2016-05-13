2e420ff5ad47e2a47bd91706b56e738ca13e6bfd
Bri in The Sky <brianh6854@googlemail.com>
2016-05-13 21:24:00 +0100

    Changed schema to test

--- examples/example_schema.sql
+++ examples/example_schema.sql
@@ -1,13 +1,13 @@
--- msqldump dump table threatmonitor.fruits Wed Apr 27 22:23:01 2016
--- MonetDB v11.21.19 (Jul2015-SP4), 'mapi:monetdb://mdb-master-01:50000/threatmonitor'
-CREATE TABLE "threatmonitor"."fruits" (
+-- msqldump dump table test.fruits Wed Apr 27 22:23:01 2016
+-- MonetDB v11.21.19 (Jul2015-SP4), 'mapi:monetdb://mdb-master-01:50000/test'
+CREATE TABLE "test"."fruits" (
 	"name"     VARCHAR(50),
 	"price"    VARCHAR(10),
 	"weight"   INTEGER,
 	"comments" CHARACTER LARGE OBJECT,
 	"id"       INTEGER
 );
-COPY 6 RECORDS INTO "threatmonitor"."fruits" FROM stdin USING DELIMITERS '\t','\n','"';
+COPY 6 RECORDS INTO "test"."fruits" FROM stdin USING DELIMITERS '\t','\n','"';
 "Apple"	"9.99"	50	NULL	1
 "Bananna"	"3.99"	30	NULL	2
 "Orange"	"7.99"	60	NULL	3
@@ -15,7 +15,7 @@ COPY 6 RECORDS INTO "threatmonitor"."fruits" FROM stdin USING DELIMITERS '\t','\
 "Kiwi"	"9.00"	20	NULL	5
 "Tomato"	"2.00"	20	"Yes a fruit"	6
 
-CREATE TABLE "threatmonitor"."guid_test" (
+CREATE TABLE "test"."guid_test" (
 	"guid" CHAR(36)      NOT NULL,
 	"f1"   CHAR(26),
 	"f2"   CHAR(26),
