56ee5f93dcd091faacf20da39cc939722f62a48b
Bri in The Sky <brianh6854@googlemail.com>
2016-07-06 09:51:31 +0100

    Added the code_test

---
+++ examples/code_test.cr
@@ -0,0 +1,34 @@
+require "crystal-monetdb-libmapi"
+require "crystal-monetdb-libmapi/monetdb_data"
+require "colorize"
+
+class ConnectionError < Exception; end
+class QueryError < Exception; end
+class InternalError < Exception; end
+class TimeoutError < Exception; end
+
+host = "127.0.0.1"
+port = 50000
+username = "monetdb"
+password = "monetdb"
+db = "threatmonitor"
+
+mero = MonetDB::ClientJSON.new
+mero.host = host
+mero.port = port
+mero.username = username
+mero.password = password
+mero.db = db
+
+myschema = "\"threatmonitor\""
+
+query = "SELECT * FROM #{myschema}.fruits;"
+mid = mero.connect
+result_json = mero.query_json(query)
+result = mero.json_to_hash(result_json)
+puts "Hash Created:".colorize(:red)
+p result
+puts "Display specific fields".colorize(:red)
+result.each {|k,v|
+  puts "Hash Key: #{k} Name: #{v["name"]} Price: #{v["price"]}"
+}
